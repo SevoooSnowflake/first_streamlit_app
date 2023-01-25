c86ecdc8f8d3991798fa13480a446ad70dabef1c
SevoooSnowflake <123158416+SevoooSnowflake@users.noreply.github.com>
2023-01-25 11:28:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit 
+import streamlit
 import pandas
 import requests
 import snowflake.connector
@@ -11,16 +11,28 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#import pandasmy_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.streamlit.dataframe(fruits_to_show)
-#Crete the repeatable code block (called a function)def get_fruityvice_data(this_fruit_choice):
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+#Crete the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
-  return fruityvice_normalized#New section to display fruityvice api responsestreamlit.header("Fruityvice Fruit Advice!")
+  return fruityvice_normalized
+
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -28,21 +40,31 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-except URLError as e:  #streamlit.write('The user entered ', fruit_choice)  streamlit.error()
-#streamlit.stop()streamlit.header("View Our Fruit List - Add Your Favorites!")
-#snowflake-related functionsdef get_fruit_load_list():
+except URLError as e:  #streamlit.write('The user entered ', fruit_choice)
+  streamlit.error()
+#streamlit.stop()
+
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+#snowflake-related functions
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall() 
-# Add a button to load the fruitif streamlit.button('Get Fruit List'):
+  
+# Add a button to load the fruit
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
-# Allow the end user to add a fruit to teh listdef insert_row_snowflake(new_fruit):
+
+# Allow the end user to add a fruit to teh list
+def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit +"')")
-    return "Thanks for adding " + new_fruitadd_my_fruit = streamlit.text_input('Waht fruit would you like to add?')
+    return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('Waht fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
@@ -51,3 +73,4 @@ if streamlit.button('Add a Fruit to the List'):
 streamlit.stop()
 streamlit.write('Thanks for adding', add_my_fruit)
 # This will nor work correctly, but just go with it for now
+
